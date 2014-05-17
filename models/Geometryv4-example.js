145043ac33f35b18e815ca7543ce69c918d964a1
mattmas <mmason@rand.com>
2014-05-17 16:14:53 -0400

    fixed empty

---
+++ models/Geometryv4-example.js
@@ -0,0 +1,11 @@
+{
+    "metadata": {
+        "version": 4,
+        "type": "geometry",
+        "generator": "GeometryExporter"
+    },
+    "vertices": [50,50,50,...],
+    "normals": [1,0,0,...],
+    "uvs": [[0,1,...]],
+    "faces": [56,0,2,1,0,1,2,0,0,0,0,...]
+}
