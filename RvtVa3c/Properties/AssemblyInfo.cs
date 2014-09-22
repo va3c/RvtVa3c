eb25bde2f0858d671d390c8c3c64d1f980c01d60
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2014-09-22 20:59:16 +0200

    adding new models

--- RvtVa3c/Properties/AssemblyInfo.cs
+++ RvtVa3c/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 // 2014-09-02 2015.0.0.19 minor cleanup before removing scene definition
 // 2014-09-03 2015.0.0.20 fixed bug in SelectFile, need to determine full output path
 // 2014-09-03 2015.0.0.21 replace top level json container Scene for Object3D
-// 2014-09-04 2015.0.0.23 added new models, theo confirmed it works, and added name property to materials
+// 2014-09-04 2015.0.0.23 added new models, theo confirmed it works, added name property to materials
 //
 [assembly: AssemblyVersion( "2015.0.0.25" )]
 [assembly: AssemblyFileVersion( "2015.0.0.25" )]
