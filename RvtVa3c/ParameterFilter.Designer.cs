481712f83c27477c0deb847c593fd66bf50317eb
Ana <anagpuyol@gmail.com>
2015-02-26 18:35:49 -0500

    Form updates

--- RvtVa3c/ParameterFilter.Designer.cs
+++ RvtVa3c/ParameterFilter.Designer.cs
@@ -31,15 +31,17 @@
             this.checkUncheck = new System.Windows.Forms.Button();
             this.export_button = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
+            this.Description = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // checkUncheck
             // 
-            this.checkUncheck.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkUncheck.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.checkUncheck.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkUncheck.Location = new System.Drawing.Point(12, 227);
+            this.checkUncheck.Location = new System.Drawing.Point(12, 447);
             this.checkUncheck.Name = "checkUncheck";
-            this.checkUncheck.Size = new System.Drawing.Size(184, 33);
+            this.checkUncheck.Size = new System.Drawing.Size(139, 33);
             this.checkUncheck.TabIndex = 4;
             this.checkUncheck.Text = "Check / Uncheck All";
             this.checkUncheck.UseVisualStyleBackColor = true;
@@ -49,7 +51,7 @@
             // 
             this.export_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.export_button.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.export_button.Location = new System.Drawing.Point(210, 227);
+            this.export_button.Location = new System.Drawing.Point(395, 447);
             this.export_button.Name = "export_button";
             this.export_button.Size = new System.Drawing.Size(93, 33);
             this.export_button.TabIndex = 5;
@@ -61,7 +63,7 @@
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelButton.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cancelButton.Location = new System.Drawing.Point(320, 227);
+            this.cancelButton.Location = new System.Drawing.Point(505, 447);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(87, 33);
             this.cancelButton.TabIndex = 6;
@@ -69,22 +71,47 @@
             this.cancelButton.UseVisualStyleBackColor = true;
             this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
+            // Description
+            // 
+            this.Description.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Description.Location = new System.Drawing.Point(9, 9);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(398, 45);
+            this.Description.TabIndex = 7;
+            this.Description.Text = "Select which parameters should be included with each category";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(159, 455);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(188, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Check / Uncheck current tab only";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // ParameterFilter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(419, 272);
+            this.ClientSize = new System.Drawing.Size(604, 492);
             this.ControlBox = false;
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.export_button);
             this.Controls.Add(this.checkUncheck);
-            this.MaximumSize = new System.Drawing.Size(900, 310);
-            this.MinimumSize = new System.Drawing.Size(435, 310);
+            this.Controls.Add(this.Description);
+            this.MaximumSize = new System.Drawing.Size(900, 530);
+            this.MinimumSize = new System.Drawing.Size(620, 530);
             this.Name = "ParameterFilter";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ParameterFilter";
+            this.Text = "Parameter Filter";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -93,6 +120,8 @@
         private System.Windows.Forms.Button checkUncheck;
         private System.Windows.Forms.Button export_button;
         private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label Description;
+        private System.Windows.Forms.CheckBox checkBox1;
 
 
 
